0bf8f49d16d43f0676d7e1765c97375000ef1b4e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-06-30 12:05:01 +0500

    Validations put on two fields

--- Task002_ForminReact/corefiles/src/App.jsx
+++ Task002_ForminReact/corefiles/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { useForm } from "react-hook-form";
 
+
+
 function App() {
   const {
     register,
@@ -9,14 +11,48 @@ function App() {
     formState: { errors },
   } = useForm();
 
+  function onSubmit(data) {
+    console.log("Submitting the form", data)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Full Name: </label>
+        <input
+          {...register("firstName", {
+            required: "This field is required",
+            minLength: {
+              value: 3,
+              message: "Name should be at least 3 characters",
+            },
+            maxLength: {
+              value: 30,
+              message: "Name should be at most 30 characters",
+            },
+          })}
+        />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
       </div>
       <div>
-        <label>Full Name: </label>
+        <label>Father Name: </label>
+         <input
+          {...register("fatherName", {
+            required: "This field is required",
+            minLength: {
+              value: 3,
+              message: "Name should be at least 3 characters",
+            },
+            maxLength: {
+              value: 30,
+              message: "Name should be at most 30 characters",
+            },
+          })}
+        />
+        {errors.fatherName && <p>{errors.fatherName.message}</p>}
       </div>
+
+      <input type="submit" value="Submit" />
     </form>
   )
 }
